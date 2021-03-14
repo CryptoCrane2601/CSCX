1def6c8146899e4fc896e537cbbf0103983bca18
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2021-03-14 20:06:39 +0100

    Added exercise_27

---
+++ exercise_27.py
@@ -0,0 +1,13 @@
+#age
+
+import sys
+
+for line in sys.stdin:
+    name, age = line.split()
+    age = int(age)
+    line = line.strip()
+    print(name + ' is ' + str(age) + ' years old.')
+
+#John is 23 years old.
+    
+    
