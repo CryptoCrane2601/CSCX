4c48c2cead212738f074d9477be3fe7a45cd1f3b
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2021-03-16 23:19:09 +0100

    Added exercise_30

---
+++ exercise_30.py
@@ -0,0 +1,25 @@
+#triple
+
+import sys
+
+def triple(x):
+   return x * 3
+
+for num in sys.stdin:
+    num = num.strip()
+    num = int(num)
+    newNum = triple(num)
+    print(newNum)
+    
+    
+        
+         
+        
+    
+
+
+
+
+
+        
+    
