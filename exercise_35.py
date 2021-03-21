e4fbe443a57bbe0d2901a7c5f5464edb64c805e4
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2021-03-21 22:08:34 +0100

    Added exercise_35

---
+++ exercise_35.py
@@ -0,0 +1,27 @@
+#box
+
+import sys
+
+def volume(w,h,d):
+    return w * h * d
+
+def area(w,h,d):
+    return 2*w*h + 2*w*d + 2*d*h
+
+for numbers in sys.stdin:
+    w, h, d = numbers.split()
+    w = int(w)
+    h = int(h)
+    d = int(d)
+    volume1 = volume(w,h,d)
+    print('The volume of a ' +str(w) + ' by ' +str(h) + ' by ' +str(d) +
+          ' box is ' +str(volume1) + '.')
+    area1 = area(w,h,d)
+    print('The surface area of a ' +str(w) + ' by ' +str(h) +
+          ' by ' +str(d) + ' box is ' +str(area1) + '.' + '\n')
+    
+
+
+
+
+
